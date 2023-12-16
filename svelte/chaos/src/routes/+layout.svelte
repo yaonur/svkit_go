2630d7eb21408f77834b430e51de330affbe8b9b
onur yanık <ya_onur@hotmail.com>
2023-12-17 01:27:38 +0300

    ss

---
+++ svelte/chaos/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css'
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+	import { setLanguageTag, sourceLanguageTag, type AvailableLanguageTag } from '$paraglide/runtime';
+	import { availableLanguageTags } from '$paraglide/runtime';
+	import { route } from '$lib/i18n-routing';
+
+	$: lang = ($page.params.lang as AvailableLanguageTag) ?? sourceLanguageTag;
+
+	$: setLanguageTag(lang);
+	$: pathName = $page.url.pathname;
+	console.log('pathName layout', $page.url.pathname);
+	if (browser && ($page.url.pathname == '/' || !availableLanguageTags.includes(lang))) {
+		// console.log("path name wrong");
+		goto(`/en`);
+	}
+</script>
+
+{#each availableLanguageTags as lang, i}
+	<a href={route($page.url.pathname, lang)} hreflang={lang}>Change language to {lang}</a>
+{/each}
+<div>
+	<a href={$page.params.lang + '/signup'}>Signup</a>
+</div>
+{#key lang}
+	<slot />
+{/key}
